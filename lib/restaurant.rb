efb8653ee9fbca3ee8a834199c5acc4cd41345dc
tekhuy <huyle20@hotmail.com>
2015-01-02 15:55:41 +0000

    twilio working and all numbers & id secure

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,15 +6,15 @@ require 'twilio-ruby'
 
 Dotenv.load
 
-# Twilio.configure do |config|
-#   config.account_sid = ENV['TWILIO_ACCOUNT_SID']
-#   config.auth_token = ENV['TWILIO_AUTH_TOKEN']
-# end
+Twilio.configure do |config|
+  config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+  config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+end
 
-client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+client = Twilio::REST::Client.new 
 
-client.account.messages.create({
-    :from => '441970800050', 
-    :to => '447899808802', 
+client.messages.create({
+    :from => ENV['TWILIO_NUMBER'], 
+    :to => ENV['CLIENT_NUMBER'], 
     :body =>'Testing Twilio'
     })
