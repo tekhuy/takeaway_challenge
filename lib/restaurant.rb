b0ab93fa4d4d7b72ab5cd24ee01dadc1837d1fa0
tekhuy <huyle20@hotmail.com>
2015-01-02 15:07:43 +0000

    created .env and .gitignore for Twilio purposes... Twilio not working

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,2 +1,20 @@
 class Restaurant
 end
+
+require 'dotenv'
+require 'twilio-ruby'
+
+Dotenv.load
+
+# Twilio.configure do |config|
+#   config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+#   config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+# end
+
+client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+client.account.messages.create({
+    :from => '441970800050', 
+    :to => '447899808802', 
+    :body =>'Testing Twilio'
+    })
